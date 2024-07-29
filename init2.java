d304833f2e5ec7ef176fc4601849ff06e5016ba0
Bogdanvolkiv <162624882+Bogdanvolkiv@users.noreply.github.com>
2024-07-29 21:00:35 +0300

    Update init2.java

--- init2.java
+++ init2.java
@@ -1,9 +1,8 @@
-// ГорячийНапиток.java
-public class HotDrink {
+public class Beverage {
     private String name;
     private int volume;
 
-    public HotDrink(String name, int volume) {
+    public Beverage(String name, int volume) {
         this.name = name;
         this.volume = volume;
     }
@@ -15,18 +14,11 @@ public class HotDrink {
     public int getVolume() {
         return volume;
     }
-
-    @Override
-    public String toString() {
-        return "HotDrink{name='" + name + "', volume=" + volume + "}";
-    }
 }
-
-// ГорячийНапитокСТемпературой.java
-public class HotDrinkWithTemperature extends HotDrink {
+public class HotBeverage extends Beverage {
     private int temperature;
 
-    public HotDrinkWithTemperature(String name, int volume, int temperature) {
+    public HotBeverage(String name, int volume, int temperature) {
         super(name, volume);
         this.temperature = temperature;
     }
@@ -37,62 +29,165 @@ public class HotDrinkWithTemperature extends HotDrink {
 
     @Override
     public String toString() {
-        return "HotDrinkWithTemperature{name='" + getName() + "', volume=" + getVolume() + ", temperature=" + temperature + "}";
+        return "HotBeverage{" +
+                "name='" + getName() + '\'' +
+                ", volume=" + getVolume() +
+                ", temperature=" + temperature +
+                '}';
     }
 }
-
-// ТорговыйАвтомат.java
 public interface VendingMachine {
-    HotDrink getProduct(String name, int volume, int temperature);
+    HotBeverage getProduct(String name, int volume, int temperature);
 }
-
-// ГорячихНапитковАвтомат.java
 import java.util.ArrayList;
 import java.util.List;
 
-public class HotDrinkVendingMachine implements VendingMachine {
-    private List<HotDrinkWithTemperature> drinks;
+public class HotBeverageVendingMachine implements VendingMachine {
+    private List<HotBeverage> inventory = new ArrayList<>();
 
-    public HotDrinkVendingMachine() {
-        drinks = new ArrayList<>();
-    }
-
-    public void addDrink(HotDrinkWithTemperature drink) {
-        drinks.add(drink);
+    public void addProduct(HotBeverage beverage) {
+        inventory.add(beverage);
     }
 
     @Override
-    public HotDrink getProduct(String name, int volume, int temperature) {
-        for (HotDrinkWithTemperature drink : drinks) {
-            if (drink.getName().equals(name) && drink.getVolume() == volume && drink.getTemperature() == temperature) {
-                return drink;
+    public HotBeverage getProduct(String name, int volume, int temperature) {
+        for (HotBeverage beverage : inventory) {
+            if (beverage.getName().equals(name) &&
+                beverage.getVolume() == volume &&
+                beverage.getTemperature() == temperature) {
+                return beverage;
             }
         }
-        return null; // Если не найдено соответствие
+        return null; // or throw an exception if product not found
+    }
+}
+import java.util.List;
+
+public interface QueueBehaviour {
+    void addPersonToQueue(Person person);
+    Person removePersonFromQueue();
+}
+
+public interface MarketBehaviour {
+    void acceptOrder(Order order);
+    void deliverOrder();
+}
+public class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
 }
 
-// Main.java
+public class Order {
+    private Person person;
+    private String product;
+
+    public Order(Person person, String product) {
+        this.person = person;
+        this.product = product;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+}
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Market implements QueueBehaviour, MarketBehaviour {
+    private Queue<Person> queue = new LinkedList<>();
+    private Queue<Order> orders = new LinkedList<>();
+
+    @Override
+    public void addPersonToQueue(Person person) {
+        queue.add(person);
+        System.out.println(person.getName() + " added to queue.");
+    }
+
+    @Override
+    public Person removePersonFromQueue() {
+        Person person = queue.poll();
+        if (person != null) {
+            System.out.println(person.getName() + " removed from queue.");
+        }
+        return person;
+    }
+
+    @Override
+    public void acceptOrder(Order order) {
+        orders.add(order);
+        System.out.println("Order accepted: " + order.getProduct() + " for " + order.getPerson().getName());
+    }
+
+    @Override
+    public void deliverOrder() {
+        Order order = orders.poll();
+        if (order != null) {
+            System.out.println("Order delivered: " + order.getProduct() + " to " + order.getPerson().getName());
+        }
+    }
+
+    public void update() {
+        // Simulate accepting and delivering orders
+        if (!queue.isEmpty() && !orders.isEmpty()) {
+            Person person = removePersonFromQueue();
+            if (person != null) {
+                Order order = orders.stream()
+                        .filter(o -> o.getPerson().equals(person))
+                        .findFirst()
+                        .orElse(null);
+                if (order != null) {
+                    orders.remove(order);
+                    deliverOrder();
+                }
+            }
+        }
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        // Создание автоматов и напитков
-        HotDrinkVendingMachine vendingMachine = new HotDrinkVendingMachine();
-        
-        HotDrinkWithTemperature tea = new HotDrinkWithTemperature("Tea", 250, 85);
-        HotDrinkWithTemperature coffee = new HotDrinkWithTemperature("Coffee", 200, 90);
-        HotDrinkWithTemperature hotChocolate = new HotDrinkWithTemperature("Hot Chocolate", 300, 80);
-        
-        // Добавление напитков в автомат
-        vendingMachine.addDrink(tea);
-        vendingMachine.addDrink(coffee);
-        vendingMachine.addDrink(hotChocolate);
-        
-        // Получение продукта
-        HotDrink requestedDrink = vendingMachine.getProduct("Tea", 250, 85);
-        if (requestedDrink != null) {
-            System.out.println("You got: " + requestedDrink);
+        HotBeverageVendingMachine vendingMachine = new HotBeverageVendingMachine();
+
+        HotBeverage tea = new HotBeverage("Tea", 250, 80);
+        HotBeverage coffee = new HotBeverage("Coffee", 200, 90);
+        HotBeverage hotChocolate = new HotBeverage("Hot Chocolate", 300, 85);
+
+        vendingMachine.addProduct(tea);
+        vendingMachine.addProduct(coffee);
+        vendingMachine.addProduct(hotChocolate);
+
+        HotBeverage requestedBeverage = vendingMachine.getProduct("Coffee", 200, 90);
+        if (requestedBeverage != null) {
+            System.out.println("Dispensed: " + requestedBeverage);
         } else {
-            System.out.println("The requested drink is not available.");
+            System.out.println("Product not found");
         }
+
+        Market market = new Market();
+
+        Person alice = new Person("Alice");
+        Person bob = new Person("Bob");
+
+        market.addPersonToQueue(alice);
+        market.addPersonToQueue(bob);
+
+        Order order1 = new Order(alice, "Tea");
+        Order order2 = new Order(bob, "Coffee");
+
+        market.acceptOrder(order1);
+        market.acceptOrder(order2);
+
+        market.update();  // Simulate processing orders
+        market.update();
     }
 }
